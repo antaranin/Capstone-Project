c8f04f3fd3ec02387ab1372346b08b1560246b21
antaranin <arin1994@gmail.com>
2016-04-26 01:45:19 +0200

    Initial commit

---
+++ app/src/main/java/example/com/capstoneproject/SuggestedApparelActivity.java
@@ -0,0 +1,15 @@
+package example.com.capstoneproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuggestedApparelActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suggested_apparel);
+    }
+}
